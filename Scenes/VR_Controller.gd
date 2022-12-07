6991212bdcdde8c54cb737616327e42e7aeac5a7
vlcoo <victorstancioiu@gmail.com>
2022-12-07 12:54:23 +0100

    Acabar Jurgen dialog + Anadir Jessie dialog timeline

--- Scenes/VR_Controller.gd
+++ Scenes/VR_Controller.gd
@@ -73,19 +73,19 @@ func _ready():
 	# warning-ignore-all:return_value_discarded
 	
 	# Get the teleport raycast node and assign it to the teleport_raycast variable.
-#	teleport_raycast = get_node("RayCast")
-#
-#	# Get the teleport mesh and assign it to the teleport_mesh variable.
-#	#
-#	# The teleport mesh is a child of the game scene so it is not effected changes in the VR controller
-#	# and so the teleport mesh can be used by both VR controllers.
-#	teleport_mesh = get_tree().root.get_node("Game/Teleport_Mesh")
-#
-#	# Set teleport_button_down variable to false.
-#	teleport_button_down = false
-#	# Make the teleport meshes invisible.
-#	teleport_mesh.visible = false
-#	teleport_raycast.visible = false
+	teleport_raycast = get_node("RayCast")
+
+	# Get the teleport mesh and assign it to the teleport_mesh variable.
+	#
+	# The teleport mesh is a child of the game scene so it is not effected changes in the VR controller
+	# and so the teleport mesh can be used by both VR controllers.
+	teleport_mesh = get_tree().root.get_node("Game/Teleport_Mesh")
+
+	# Set teleport_button_down variable to false.
+	teleport_button_down = false
+	# Make the teleport meshes invisible.
+	teleport_mesh.visible = false
+	teleport_raycast.visible = false
 	
 	# Get the grab area node, the grab raycast node, and the grab position node. Assign these nodes to
 	# their respective variables.
