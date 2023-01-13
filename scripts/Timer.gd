a1f3d062f4a58f31fb4655952e5a5fbe0c6dc3e4
Marta Lendínez <95087997+culfri09@users.noreply.github.com>
2023-01-13 14:11:23 +0100

    Fix timer

--- scripts/Timer.gd
+++ scripts/Timer.gd
@@ -1,24 +1,31 @@
-extends RichTextLabel
+extends Control
 
-var ms = 0
-var s= 0
-var m= 0
+export (int) var minutes = 0
+export (int) var seconds = 0
+var dsec = 0
 
-func _process(delta):
+func _physics_process(delta):
+	if seconds > 0 and dsec <= 0:
+		seconds -=1
+		dsec = 10
+	if minutes > 0 and seconds <= 0:
+		minutes -=1
+		seconds = 60
 	
-	if ms > 9 :
-		s +=1
-		ms = 0
-		
-	if s>59:
-		m+=1
-		s=0
-		
-	set_text(str(m)+":"+str(s)+":"+str(ms))
-	
-	pass 
-	
+	if seconds >= 10:
+		$sec.set_text(str(seconds))
+	else:
+		$sec.set_text("0"+str(seconds))
+	if dsec >= 10:
+		$dsec.set_text(str(dsec))
+	else:
+		$dsec.set_text("0"+str(dsec))
+	if minutes >= 10:
+		$min.set_text(str(minutes))
+	else:
+		$min.set_text("0"+str(minutes))
 
 func _on_Timer_timeout():
-	ms+=1
-	pass # Replace with function body.
+	dsec -= 1
+	if(minutes ==0 and seconds == 0):
+		get_tree().change_scene("res://Scenes/Main_VR_GUI.tscn")
--- scripts/UIManager.gd
+++ scripts/UIManager.gd
@@ -16,5 +16,4 @@ func _ready():
 #	pass
 
 
-func _on_Timer_timeout():
-	get_tree().change_scene("res://Scenes/UI/MainMenu.tscn")
+
