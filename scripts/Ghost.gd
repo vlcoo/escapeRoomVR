823d650e0f95d2388658247baed9e7814887dcf5
VlC <victorstancioiu@gmail.com>
2023-01-17 12:58:19 +0100

    Some UI adjustments

--- scripts/Ghost.gd
+++ scripts/Ghost.gd
@@ -1,10 +1,21 @@
 extends Spatial
 
 var diag_ui
+export var diag_timeline_name = "Test"
 
 
 func _ready():
 	$VRGUI/GUI_Mesh.set_transparent(true)
-	diag_ui = Dialogic.start('Test')
+
+func start_diag():
+	diag_ui = Dialogic.start(diag_timeline_name)
 	$VRGUI/GUI.add_child(diag_ui, true)
 	$VRGUI/GUI_Mesh.render_viewport()
+
+
+func _on_AreaDialog_area_entered(area: Area):
+	start_diag()
+
+
+func _on_AreaDialog_area_exited(area: Area):
+	diag_ui.queue_free()
