10664cc7dbb676d50ce686d104d7d796edde21b6
Marta Lendínez <95087997+culfri09@users.noreply.github.com>
2023-01-20 17:55:18 +0100

    Clues input implementation

---
+++ scripts/UI/InputDominos.gd
@@ -0,0 +1,57 @@
+extends RichTextLabel
+
+
+#export (int) var input = 0
+func _process(delta):
+	if(text.length()>=4):
+		text = text.substr(0,4)
+		
+	if(text == str(4651)):
+		get_tree().change_scene("res://Escape_Room_Scenes/Game.tscn")
+		
+	if(text.length()==4):
+		if(text != str(4651)):
+			$"../Warning".text = str("Incorrect!")
+
+func _on_But_1_pressed():
+	
+	#set_text(str("1"))
+	text+= str("1")
+		
+
+
+func _on_But_2_pressed():
+
+	text+= str("2")
+
+func _on_But_3_pressed():
+
+	text+= str("3")
+
+func _on_But_4_pressed():
+
+	text+= str("4")
+
+func _on_But_5_pressed():
+
+	text+= str("5")
+
+func _on_But_6_pressed():
+
+	text+= str("6")
+
+func _on_But_7_pressed():
+
+	text+= str("7")
+
+func _on_But_8_pressed():
+
+	text+= str("8")
+
+func _on_But_9_pressed():
+
+	text+= str("9")
+	
+
+func _on_Eliminate_pressed():
+	text = text.left(text.length() - 1)
---
+++ scripts/UI/InputRoman.gd
@@ -0,0 +1,57 @@
+extends RichTextLabel
+
+
+#export (int) var input = 0
+func _process(delta):
+	if(text.length()>=4):
+		text = text.substr(0,4)
+		
+	if(text == str(1551)):
+		get_tree().change_scene("res://Escape_Room_Scenes/Game.tscn")
+		
+	if(text.length()==4):
+		if(text != str(1551)):
+			$"../Warning".text = str("Incorrect!")
+
+
+	
+
+func _on_Eliminate_pressed():
+	text = text.left(text.length() - 1)
+
+
+func _on_Button_pressed():
+	text+= str("1")
+
+
+func _on_Button2_pressed():
+	text+= str("2")
+
+
+func _on_Button3_pressed():
+	text+= str("3")
+
+
+func _on_Button4_pressed():
+	text+= str("4")
+
+
+func _on_Button5_pressed():
+	text+= str("5")
+
+
+func _on_Button6_pressed():
+	text+= str("6")
+
+
+func _on_Button7_pressed():
+	text+= str("7")
+
+
+func _on_Button8_pressed():
+	text+= str("8")
+
+
+func _on_Button9_pressed():
+	text+= str("9")
+
