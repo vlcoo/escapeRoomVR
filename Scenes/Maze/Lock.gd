6d794ec092f44d1a89582f76fd119ebe4649fdce
Navarro Lazaro Javier <jnl941@inlumine.ual.es>
2023-01-24 18:36:59 +0100

    Lock animation + "unlock()" callable function

---
+++ Scenes/Maze/Lock.gd
@@ -0,0 +1,28 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("idle")
+	pass # Replace with function body.
+
+func unlock() -> void:
+	$AnimationPlayer.play("open");
+
+func activate_gravity() -> void:
+	can_fall = true
+
+var can_fall: bool = false
+var velocity: float = 0
+export(float) var grav_scale: float = 9.8
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(can_fall):
+		translation.y -= velocity*delta
+		velocity += grav_scale*delta
+	pass
